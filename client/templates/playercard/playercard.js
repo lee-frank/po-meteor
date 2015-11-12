fe6a0190eb541e0e90f172ac40f1dad5d4d80b54
Frank Lee <lee.frankie@gmail.com>
2015-11-12 14:28:13 -0500

    last login date added

--- client/templates/playercard/playercard.js
+++ client/templates/playercard/playercard.js
@@ -1,2 +1,8 @@
+//This function is available globally
+UI.registerHelper('formatTime', function(context, options) {
+  if(context) {
+	return moment(context).startOf('hour').fromNow(); 
+  }
+});
 
 
--- client/templates/playerform/playerform.js
+++ client/templates/playerform/playerform.js
@@ -13,6 +13,7 @@ Template.playerform.events({
 	    var bnetid = $('[name="bnetid"]').val();
 	    var race = $('[name="race"]').val();
 
+	    //Find out if it's an existing player
 	    var playerExists = PlayersList.findOne({
 	    	name:name,
 	    	bnetid:bnetid
@@ -20,8 +21,16 @@ Template.playerform.events({
 
 	    if (playerExists) {
 	    	Session.setPersistent('playeridSession',playerExists._id);
+	    	var playerSession = Session.get('playeridSession'); 
+   			
+	   		//Update record
+	  		PlayersList.update({_id: playerSession}, {$set: {
+	  				race: race,
+	  				loginDate: new Date()
+	  			}
+	  		});
 
-   			Materialize.toast('Existing ID found', 4000);
+	  		Materialize.toast('Existing ID found', 4000);
 	    }
 	    else {
 		    PlayersList.insert({
@@ -30,12 +39,13 @@ Template.playerform.events({
 		        race: race,
 		        rank: "Unknown Rank",
 		        description: "I'm a fun and honest " + race + " looking for another funny, kind, and honest " + race +" to make sweet Archon Mode magic with.",
-		        createdAt: new Date()
+		        createdAt: new Date(),
+		        loginDate: new Date()
 			}, function(error){
 				if(error){
 			      console.log(error.reason); // Output error if submission fails
 			    } else {
-			      //lookup document of record entered
+			      //lookup id of record entered
 			      var playerid = PlayersList.findOne({
 			      	name: name,
 			      	bnetid: bnetid,
@@ -44,7 +54,6 @@ Template.playerform.events({
 			      
 			      //set persistent session of user
 			      Session.setPersistent('playeridSession',playerid._id);
-
 			      Materialize.toast('Player card entered', 4000);
 			    }
 			});
